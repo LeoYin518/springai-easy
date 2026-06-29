5322813b776532ca323f62caab56a533fd885ff6
leoyin <yinzhengnan2021@163.com>
2026-06-29 15:50:38 +0800

    feat:RedisChatMemoryRepository实战

--- chatclient/src/main/java/com/leo/springai/chatclient/config/ChatMemoryConfig.java
+++ chatclient/src/main/java/com/leo/springai/chatclient/config/ChatMemoryConfig.java
@@ -1,9 +1,10 @@
 package com.leo.springai.chatclient.config;
 
+import com.alibaba.cloud.ai.memory.redis.RedisChatMemoryRepository;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,19 +16,49 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ChatMemoryConfig {
+
+    @Value("${spring.ai.memory.redis.host}")
+    String host;
+    @Value("${spring.ai.memory.redis.port}")
+    int port;
+    @Value("${spring.ai.memory.redis.timeout}")
+    int timeout;
+    @Value("${spring.ai.memory.redis.password}")
+    String password;
+
     @Bean
-    ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+    public RedisChatMemoryRepository redisChatMemoryRepository() {
+        return RedisChatMemoryRepository.builder()
+                .host(host)
+                .password(password)
+                .port(port)
+                .timeout(timeout)
+                .build();
+    }
+
+    @Bean
+    ChatMemory chatMemory(RedisChatMemoryRepository repository) {
         return MessageWindowChatMemory
                 .builder()
-                .maxMessages(3) // 只存 10 条记忆，淘汰策略：先进先出
+                .maxMessages(10) // 只存 10 条记忆，淘汰策略：先进先出
                 .chatMemoryRepository(repository)
                 .build();
     }
+
+//    @Bean
+//    ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
+//        return MessageWindowChatMemory
+//                .builder()
+//                .maxMessages(10) // 只存 10 条记忆，淘汰策略：先进先出
+//                .chatMemoryRepository(repository)
+//                .build();
+//    }
+
 //    @Bean
 //    ChatMemory chatMemory(ChatMemoryRepository repository) {
 //        return MessageWindowChatMemory
 //                .builder()
-//                .maxMessages(3) // 只存 10 条记忆，淘汰策略：先进先出
+//                .maxMessages(10) // 只存 10 条记忆，淘汰策略：先进先出
 //                .chatMemoryRepository(repository)
 //                .build();
 //    }
