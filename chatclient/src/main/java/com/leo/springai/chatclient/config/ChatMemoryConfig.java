031e6e941b7d55a1e17c8e11ab0b382a62f4b6e8
leoyin <yinzhengnan2021@163.com>
2026-06-29 15:31:23 +0800

    feat:JdbcChatMemoryRepository实战

--- chatclient/src/main/java/com/leo/springai/chatclient/config/ChatMemoryConfig.java
+++ chatclient/src/main/java/com/leo/springai/chatclient/config/ChatMemoryConfig.java
@@ -3,6 +3,7 @@ package com.leo.springai.chatclient.config;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +16,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatMemoryConfig {
     @Bean
-    ChatMemory chatMemory(ChatMemoryRepository repository) {
+    ChatMemory chatMemory(JdbcChatMemoryRepository repository) {
         return MessageWindowChatMemory
                 .builder()
                 .maxMessages(3) // 只存 10 条记忆，淘汰策略：先进先出
                 .chatMemoryRepository(repository)
                 .build();
     }
+//    @Bean
+//    ChatMemory chatMemory(ChatMemoryRepository repository) {
+//        return MessageWindowChatMemory
+//                .builder()
+//                .maxMessages(3) // 只存 10 条记忆，淘汰策略：先进先出
+//                .chatMemoryRepository(repository)
+//                .build();
+//    }
 }
